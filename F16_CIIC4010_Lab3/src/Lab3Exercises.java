a1df4224e677c7c822cc7de642eff2795465a2aa
JairoPC <JairoPC@192.168.0.140>
2016-09-01 03:05:49 -0400

    Oval and Polygons added

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Jairo Rosado Soto");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(800, 400);
 		myFrame.setSize(300, 300);
 		MyPanelClass myPanel = new MyPanelClass();
