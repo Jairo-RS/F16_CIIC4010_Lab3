6b05992de754e431e6820a6b583217dc0e2e369e
JairoPC <JairoPC@192.168.0.140>
2016-09-07 02:39:22 -0400

    Added the Puerto Rican Flag; fixed diagonal lines and oval

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -4,8 +4,9 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Jairo Rosado Soto");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(800, 400);
-		myFrame.setSize(300, 300);
+		//myFrame.setLocation(800, 400); // Home Desktop
+		myFrame.setLocation(583, 284); // Laptop
+		myFrame.setSize(300, 200);
 		MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
